0b7b5c50fdb2f35040772b3fd30773cfc903dded
LittleYay-54 <suvan.sunkavalli@gmail.com>
2025-01-30 08:54:23 -0600

    cleared old code from sLaTeX, moved it over to storage.py

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from gui import MainWindow
-
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    win = MainWindow()
-    win.show()
-    sys.exit(app.exec_())
+    pass
