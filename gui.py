7b690200c52e103be01fca51681a54522eaa4b30
LittleYay-54 <suvan.sunkavalli@gmail.com>
2025-02-04 08:38:22 -0600

    removed another unnecessary import statement

--- gui.py
+++ gui.py
@@ -137,7 +137,6 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     import sys
-    from PyQt5.QtWidgets import QApplication
 
     app = QApplication(sys.argv)
     window = MainWindow()
